eef110db335a89bc9179e6ebc967d9bc3f27e432
antares <antares@alyr.dev>
2024-11-20 23:49:01 +0800

    Add plasma

--- kde.nix
+++ kde.nix
@@ -8,7 +8,6 @@
 {
   lib.mkForce = {
     environment.variables = {
-      #GTK_IM_MODULE = "fcitx";
       GTK_IM_MODULE = "wayland";
       QT_IM_MODULE = "fcitx";
       XMODIFIERS = "@im=fcitx";
@@ -18,6 +17,81 @@
   };
   nixpkgs.overlays = [
     breezeOverlay
-    # (import ./breeze_overlay.nix)
   ];
+
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      wayland = {
+        enable = true;
+        compositor = "kwin";
+      };
+    };
+  };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-chinese-addons
+        fcitx5-gtk
+      ];
+      waylandFrontend = true;
+      plasma6Support = true;
+    };
+  };
+  i18n.supportedLocales = [
+    "zh_CN.UTF-8/UTF-8"
+    "en_US.UTF-8/UTF-8"
+  ];
+  i18n.defaultLocale = "en_US.UTF-8";
+  environment.systemPackages = [
+    (pkgs.haruna.overrideAttrs {
+      postFixup = ''
+        wrapProgram $out/bin/haruna \
+          --set LD_PRELOAD "${pkgs.vulkan-loader}/lib/libvulkan.so"
+      '';
+    })
+  ];
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      vaapiVdpau
+    ];
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    powerManagement.enable = true;
+
+    powerManagement.finegrained = false;
+
+    open = true;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
 }
